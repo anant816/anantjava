2a44dc970a5bcd0a85e2d882dafbc20383a423f7
anant816 <naharanant04@gmail.com>
2024-11-17 08:17:05 +0530

    Add files via upload

---
+++ assignment_5.java
@@ -0,0 +1,23 @@
+class Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barking");
+    }
+}
+class babydog extends Dog{
+    void weep(){
+        System.out.println("Weeping");
+    }
+}
+public class Assignment2_13 {
+    public static void main(String[] args) {
+        babydog dog=new babydog();
+        dog.weep();
+        dog.bark();
+        dog.eat();
+    }
+}
